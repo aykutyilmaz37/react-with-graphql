f83679b66010319a14511c7a3bc2668f5bdb97e7
Aykut <aykutyilmaz37@gmail.com>
2020-12-30 21:20:45 +0300

    app.js

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(
 );
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend/build/index.html'));
+  res.sendFile(path.resolve(__dirname + 'frontend/build/index.html'));
 });
 
 mongoose
